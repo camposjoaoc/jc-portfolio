80c435b7388e4d17ddd4e7b47230172c9abb79b7
João Canabarro Campos <joaocampos963@icloud.com>
2025-01-29 13:02:58 -0300

    Fix routes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jc-portfolio',
+  base: '/jc-portfolio/',
   server: {
     host: true,
-  },
-  devServer: {
     historyApiFallback: true,
   },
 })
