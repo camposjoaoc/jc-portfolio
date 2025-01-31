8edbc8dd1df9e83761df69ef5a9b0117ee149de8
João Canabarro Campos <joaocampos963@icloud.com>
2025-01-31 17:44:15 -0300

    Small fixes in some pages + add new favicon

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -7,12 +7,13 @@ function Contact() {
     const [feedbackMessage, setFeedbackMessage] = useState("");
     const [showForm, setShowForm] = useState(true);
     const [state, handleSubmit] = useForm("mnnjvpdb"); //formspree data
+    const [copied, setCopied] = useState(false); // copy mail
 
     useEffect(() => {
         if (state.succeeded) {
             console.log("Ok");
             setShowForm(false); // form ocult
-            setFeedbackMessage("Mensagem enviada com sucesso!");
+            setFeedbackMessage("Message sent successfully!");
 
             const timer = setTimeout(() => {
                 setShowForm(true); // After 10 seconds, form showing
@@ -23,13 +24,29 @@ function Contact() {
         }
     }, [state.succeeded]);
 
+    // Função para copiar o e-mail ao clicar
+    const handleCopyEmail = () => {
+        const email = "joaocanabarrocampos@gmail.com";
+        navigator.clipboard.writeText(email).then(() => {
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000); // Resetar "Copiado!" após 2s
+        });
+    };
+
     return (
         <div className="container mid-container">
             <section>
                 <div className="container-contact">
                     <h1 className="h1-title">CONTACT</h1>
                     <p className="p-info">Feel free to reach out to me at:</p>
-                    <p className="p-mail">joaocanabarrocampos@gmail.com</p>
+                    {/* Paragraph that copies the email when clicked*/}
+                    <p
+                        className="p-mail"
+                        onClick={handleCopyEmail}
+                        title="Click to copy"
+                    >
+                        {copied ? "Copied!" : "joaocanabarrocampos@gmail.com"}
+                    </p>
                     <p className="p-info">or fill out the form below:</p>
                     {showForm ? (
                         <form onSubmit={handleSubmit} className="form-content">
